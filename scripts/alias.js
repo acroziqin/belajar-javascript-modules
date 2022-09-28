df9ce71cbc4734fb3066a2d9abbed5fd30e99afe
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-28 14:25:51 +0700

    Alias

---
+++ scripts/alias.js
@@ -0,0 +1,10 @@
+const company = "Krisna Beaute";
+
+function sum(first, second) {
+    return first + second;
+}
+
+class Company {}
+
+// export { company as perusahaan, sum as total, Company as Perusahaan };
+export { company, sum, Company };
