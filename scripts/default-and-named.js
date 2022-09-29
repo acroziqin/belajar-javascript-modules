3d7abd99f74b9401d8c59095fd7ac72a56a1c18b
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-29 08:59:57 +0700

    Export Default

---
+++ scripts/default-and-named.js
@@ -0,0 +1,8 @@
+export const title = "Krisna Beaute";
+
+const content = "Belajar JavaScript Module"
+
+const author = "Achmad Choirur Roziqin"
+
+export {content}
+export default author
---
+++ scripts/default-variable.js
@@ -0,0 +1,1 @@
+export default name = "Achmad Choirur Roziqin";
