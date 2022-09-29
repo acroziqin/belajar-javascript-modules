56812394b6988bb736aaa63e0ebf50697da9a32a
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-29 10:34:18 +0700

    Module Object

---
+++ scripts/module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Achmad";
+export const middleName = "Choirur";
+export const lastName = "Roziqin";
+
+export function hello() {
+    console.info("Hello from Roziqin");
+}
+
+export class Person {
+    constructor(name) {
+        this.name = "Roziqin";
+    }
+}
